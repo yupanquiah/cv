8f11000b468eff57e36382fd4beceb0d00c2865e
Humberto Yupanqui Apaza <yupanquiah@gmail.com>
2025-03-21 10:21:08 -0500

    add font dependencies and update CV profiles; refactor component imports and styles

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,1 +1,11 @@
-export type SocialIcon = Record<string, string | any>;
+export type SocialIcon = Record<string, string | any>
+
+export interface Info {
+  id: string
+  section: string
+  title: string
+  url: string
+  icon: string
+  hotkey: string
+  handler?: () => void
+}
