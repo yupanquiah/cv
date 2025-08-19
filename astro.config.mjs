e7d2414ca1fb227fac1ee12d8042fcfe367b0cc1
Humberto Yupanqui Apaza <yupanquiah@gmail.com>
2025-08-19 14:28:32 -0500

    Add font support and refactor components for improved typography

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,37 @@
 // @ts-check
-import { defineConfig } from 'astro/config'
+import { defineConfig, fontProviders } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
   build: {
     inlineStylesheets: 'always',
+  },
+
+  experimental: {
+    fonts: [
+      {
+        name: "Geist",
+        cssVariable: "--font-geist",
+        provider: fontProviders.fontsource(),
+        weights: ["100 900"],
+        display: "swap"
+      },
+      {
+        name: "Monaspace Neon",
+        cssVariable: "--font-monaspace-neon",
+        provider: fontProviders.fontsource(),
+        weights: [300, 400, 500],
+        display: "swap"
+      },
+      {
+        name: "Monaspace Radon",
+        cssVariable: "--font-monaspace-radon",
+        provider: fontProviders.fontsource(),
+        weights: [400],
+        styles: ["italic"],
+        display: "swap"
+      }
+    ]
   },
 
   site: "https://cv.yupanqui.dev/"
